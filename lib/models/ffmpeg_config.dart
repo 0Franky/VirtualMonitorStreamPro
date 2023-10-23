bef77aa9c9794003d52804d37f570ff578999b78
0Franky <frhaexin@gmail.com>
2023-10-23 09:09:55 +0200

    update

--- lib/models/ffmpeg_config.dart
+++ lib/models/ffmpeg_config.dart
@@ -13,7 +13,6 @@ class FfmpegConfig {
   final String hwAccelerationOptionalConfig;
   final String videoCodec;
 
-  final int qualityIndex;
   final String rate;
   final String bufsize;
   final String optionalParams;
@@ -36,7 +35,6 @@ class FfmpegConfig {
     this.framerate = DEFAULT_REFRASH_RATE,
     this.width = DEFAULT_WIDTH,
     this.height = DEFAULT_HEIGHT,
-    this.qualityIndex = 26,
     this.rate = "2000K",
     this.bufsize = "512k",
     this.optionalParams = "",
@@ -48,7 +46,7 @@ class FfmpegConfig {
   });
 
   String get ffmpegConfigParams =>
-      '-f $ffmpegGrubber -s ${width}x$height -framerate $framerate $displayOptions ${useHWAcceleration ? '$hwAccelerationDeviceConfig $hwAccelerationOptionalConfig' : ''} -i $display -c:v $videoCodec ${useHWAcceleration ? '-qp:v $qualityIndex -b:v $rate -minrate $rate' : ''} -bf 0 $optionalParams ${useHWAcceleration ? '-maxrate $rate' : ''} -bufsize $bufsize -f rawvideo $STREAM_PROTOCOL://$DEFAULT_CLIENT_IP:$DEFAULT_PORT';
+      '${useHWAcceleration ? '$hwAccelerationDeviceConfig $hwAccelerationOptionalConfig' : ''} -f $ffmpegGrubber -s ${width}x$height -framerate $framerate $displayOptions -i $display -c:v $videoCodec $optionalParams ${useHWAcceleration ? '-maxrate $rate' : ''} -bufsize $bufsize -f rawvideo $STREAM_PROTOCOL://$DEFAULT_CLIENT_IP:$DEFAULT_PORT';
 
   String get ffmpegConfigCmd =>
       '$ffmpegPath$ffmpegProgramFile $ffmpegConfigParams';
@@ -69,7 +67,7 @@ class FfmpegConfig {
       videoCodec: "libx264",
       framerate: framerate,
       optionalParams:
-          "-profile:v baseline -trellis 0 -subq 1 -level 32 -preset superfast -tune zerolatency -crf 30 -threads 0 -refs 4 -coder 0 -b_strategy 0 -sc_threshold 0 -x264-params vbv-maxrate=2000:slice-max-size=1500:keyint=30:min-keyint=10: -pix_fmt yuv420p -an",
+          "-profile:v baseline -trellis 0 -subq 1 -level 32 -preset superfast -tune zerolatency -crf 30 -threads 0 -refs 4 -coder 0 -b_strategy 0 -bf 0 -sc_threshold 0 -x264-params vbv-maxrate=2000:slice-max-size=1500:keyint=30:min-keyint=10: -pix_fmt yuv420p -an",
       displayOptions: displayOptions,
     );
   }
--- lib/utils/server/server_linux.dart
+++ lib/utils/server/server_linux.dart
@@ -18,9 +18,9 @@ class ServerLinux implements ServerPlatformInterface {
 
   @override
   List<FfmpegConfig> Internal_GetServerFfmpegConfig() => [
-        getHwAccConfig("cuda"),
-        getHwAccConfig("cuda"),
-        getHwAccConfig("vaapi"),
+        getVaapiConfig(),
+        getCudaConfig(videoCodec: "hevc_nvenc", optionalParams: "-profile 0"),
+        getCudaConfig(videoCodec: "h264_nvenc"),
       ];
 
   FfmpegConfig Internal_GetServerFfmpegCpuConfig() {
@@ -43,7 +43,10 @@ class ServerLinux implements ServerPlatformInterface {
     );
   }
 
-  FfmpegConfig getHwAccConfig(String hwAcc) {
+  FfmpegConfig getVaapiConfig({
+    int qualityIndex = 26,
+    String rate = "2000K",
+  }) {
     return FfmpegConfig(
       useHWAcceleration: true,
       ffmpegProgramFile: ffmpegProgramFile,
@@ -53,7 +56,26 @@ class ServerLinux implements ServerPlatformInterface {
       hwAccelerationDeviceConfig:
           '-vf \'format=nv12,hwupload,scale_vaapi=w=$DEFAULT_WIDTH:h=$DEFAULT_HEIGHT\'',
       videoCodec: "h264_vaapi",
-      optionalParams: "-preset superfast -tune zerolatency",
+      rate: rate,
+      optionalParams:
+          "-qp:v $qualityIndex -bf 0 -b:v $rate -minrate $rate -bf 0 -preset superfast -tune zerolatency",
+    );
+  }
+
+  FfmpegConfig getCudaConfig({
+    required String videoCodec,
+    String optionalParams = "",
+  }) {
+    return FfmpegConfig(
+      useHWAcceleration: true,
+      ffmpegProgramFile: ffmpegProgramFile,
+      ffmpegGrubber: ffmpegGrubber,
+      display: ":0.0",
+      hwAccelerationOptionalConfig: "-vaapi_device /dev/dri/renderD128",
+      hwAccelerationDeviceConfig: '-hwaccel cuda -hwaccel_output_format cuda',
+      videoCodec: videoCodec,
+      optionalParams:
+          "$optionalParams -zerolatency 1 -delay:v 0 -preset p1 -tune ull -qmin 0 -temporal-aq 1 -rc-lookahead 0 -i_qfactor 0.75 -b_qfactor 1.1",
     );
   }
 }
