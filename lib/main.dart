ba5cc67e8d2a72f549ce46f924056e8d2d2e5875
0Franky <frhaerxin@gmail.com>
2024-01-23 12:06:24 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 // import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:universal_platform/universal_platform.dart';
+import 'package:video_player_media_kit/video_player_media_kit.dart';
 import 'package:virtual_monitor_stream_pro/consts/strings.dart';
 import 'package:virtual_monitor_stream_pro/screens/home_page_screen.dart';
 import 'package:virtual_monitor_stream_pro/style/theme.dart';
-import 'package:window_manager/window_manager.dart';
+// import 'package:window_manager/window_manager.dart';
 
 void main() async {
   await prepareApp();
@@ -14,8 +16,17 @@ void main() async {
 Future<void> prepareApp() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await windowManager.ensureInitialized();
+  if (UniversalPlatform.isDesktop) {
+    // await windowManager.ensureInitialized();
+  }
   MediaKit.ensureInitialized();
+  VideoPlayerMediaKit.ensureInitialized(
+    android: true,          // default: false    -    dependency: media_kit_libs_android_video
+    // iOS: true,              // default: false    -    dependency: media_kit_libs_ios_video
+    // macOS: true,            // default: false    -    dependency: media_kit_libs_macos_video
+    windows: true,          // default: false    -    dependency: media_kit_libs_windows_video
+    linux: true,            // default: false    -    dependency: media_kit_libs_linux
+  );
 }
 
 class VirtualMonitorStreamPro extends StatefulWidget {
